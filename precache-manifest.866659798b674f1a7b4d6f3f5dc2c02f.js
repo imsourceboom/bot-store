c41c9111c49fdb49179b4ad2c8ea9014fa15d061
imsourceboom <ecmascript.developer@gmail.com>
2019-02-01 16:28:13 +0900

    Updates

---
+++ precache-manifest.866659798b674f1a7b4d6f3f5dc2c02f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "43d8e368a16d2162e4b2",
+    "url": "/bot-store/static/js/runtime~main.43d8e368.js"
+  },
+  {
+    "revision": "9d184c332816e0ce5957",
+    "url": "/bot-store/static/js/main.9d184c33.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/bot-store/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "89268865df68f31e12f900cae48297a0",
+    "url": "/bot-store/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/bot-store/precache-manifest.866659798b674f1a7b4d6f3f5dc2c02f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/bot-store/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

37b63ef7f6a3797143f16c4c09945d7b7a650bb3
imsourceboom <ecmascript.developer@gmail.com>
2019-02-10 16:43:35 +0900

    routeing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import GlobalStyle from 'style/GlobalStyle';
+import styled from 'styled-components';
+
+import Nav from 'components/Nav';
+import { Story, Game, Dapp, Bot, Search } from 'pages';
 
 class App extends Component {
     render() {
-        return <Nav />;
+        return (
+            <>
+                <GlobalStyle />
+                <Nav />
+                <Main>
+                    <Switch>
+                        <Route exact path="/" component={Story} />
+                        <Route path="/game" component={Game} />
+                        <Route path="/dapp" component={Dapp} />
+                        <Route path="/bot" component={Bot} />
+                        <Route path="/search" component={Search} />
+                    </Switch>
+                </Main>
+            </>
+        );
     }
 }
 
-const Nav = () => {
-    return <span>BOT</span>;
-};
+const Main = styled.main`
+    & > section {
+        max-width: 1025px;
+        margin: 0 auto;
+        padding: 0 1rem 100px;
+        animation: trigger 0.5s;
+
+        @media (min-width: 1025px) {
+            & {
+                background-color: red;
+            }
+        }
+
+        @keyframes trigger {
+            from {
+                opacity: 0;
+            }
+            to {
+                opacity: 1;
+            }
+        }
+    }
+`;
 
 export default App;

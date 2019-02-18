f5273fc956401a8e20a1f85668f1c9350d97dec2
imsourceboom <ecmascript.developer@gmail.com>
2019-02-18 14:26:03 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bot-store/precache-manifest.5fe91202e000c76f7cbd033826be7584.js"
+  "/bot-store/precache-manifest.ac42b86ea0484afe56b18b9c9be71059.js"
 );
 
 workbox.clientsClaim();
